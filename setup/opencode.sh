6be138a702358495a71d489c403c3a00aac27df0
Aidan Lynch <aidantlynch00@gmail.com>
2025-11-05 20:44:17 -0500

    Change opencode alias and function to use full path to opencode binary

--- setup/opencode.sh
+++ setup/opencode.sh
@@ -1,11 +1,11 @@
 #.bashrc
-alias oc="opencode"
+alias oc="$HOME/.tool-bin/current/bin/opencode"
 
 function oct {
     tmp_dir=$(mktemp --directory)
     prev_dir=$(pwd)
     cd "$tmp_dir"
-    opencode $@
+    $HOME/.tool-bin/current/bin/opencode $@
     cd "$prev_dir"
 
     if ! rmdir "$tmp_dir" 1>/dev/null 2>&1; then
